81047c2108067f616eb5dbc5ba1dc17298c85d98
Daga <33626572+dagaaaaa@users.noreply.github.com>
2018-08-28 20:35:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
